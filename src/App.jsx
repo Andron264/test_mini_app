aeaec305afa5ac3faec8a4c674d68d9e08e96291
Andron264 <anzopryso@gmail.com>
2026-07-02 00:46:47 +0300

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { useState } from "react";
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const number = 8;
-  const name = 'Andrei';
-  const age = 20;
-  const city = 'Minsk';
-  const [show, setShow] = useState(false)
   return (
-    <div>
-      <h1>📋 My Tasks</h1>
-
-      <p>Количество задач: {count}</p>
-      <p>Имя: {name}</p>
-      <p>Возраст: {age}</p>
-      <p>Горол: {city}</p>
-      <p>{number ** 2}</p>
-
-      <button onClick={() => setCount(count + 1)}>
-        Добавить задачу
-      </button>
-      <button onClick={() => setCount(count - 1)}>
-        Удалить задачу
-      </button>
-      <p>Rreact</p>
-      {show && <p>ОФРВЛОЫЛФДЛВТФЫДВФДЫТДВЛФТЫДЛФЫ</p>}
-      <button onClick={() => setShow(!show)}>{show && <p>Скрыть Описание</p>}{!show && <p> Открыть Описание</p>}</button>
+    <div className="app">
+      <Header />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="header">
+            <h1>👋 Good morning, Andrei</h1>
+            <p>You have 4 tasks today</p>
+        </header>
+    );
+}
+
+export default Header;
